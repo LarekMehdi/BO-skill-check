e7145992ab8b6acda79468764266a9eaa6dd1d8a
mehdi-larek <larek.med@gmail.com>
2025-07-21 16:14:05 +0200

    init

---
+++ src/test/java/fr/perso/skillcheck/SkillCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.perso.skillcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

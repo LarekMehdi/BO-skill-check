c0321c4eedf72feee2f221aa7d346a429f68debe
mehdi-larek <larek.med@gmail.com>
2025-07-26 14:08:15 +0200

    cors policy + correction de la récup d'un user avec son psuedo

--- src/main/java/fr/perso/skillcheck/security/SecurityConfig.java
+++ src/main/java/fr/perso/skillcheck/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,10 +20,20 @@ public class SecurityConfig {
 
     @Autowired
     private JwtAuthenticationFilter     jwtAuthenticationFilter;
+
+    @Autowired
+    private CustomUserDetailsService    customUserDetailsService;
     
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(customUserDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
     }
 
     @Bean
@@ -47,7 +58,4 @@ public class SecurityConfig {
         return config.getAuthenticationManager();
     }
 
-    
-
-    
 }

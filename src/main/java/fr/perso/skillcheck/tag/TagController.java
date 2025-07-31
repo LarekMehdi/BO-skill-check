fa1ec0f3ce999cf915ab8761e99219e17d9fb463
mehdi-larek <larek.med@gmail.com>
2025-07-31 14:52:49 +0200

    custom 404 + REST pour les tags

--- src/main/java/fr/perso/skillcheck/tag/TagController.java
+++ src/main/java/fr/perso/skillcheck/tag/TagController.java
@@ -1,7 +1,13 @@
 package fr.perso.skillcheck.tag;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +22,39 @@ public class TagController {
     @Autowired 
     private TagService  tagService;
 
+    /** FIND ALL **/
+
+    @GetMapping()
+    public List<Tag> findAll() {
+        return this.tagService.findAll();
+    }
+
+    /** FIND **/
+
+    @GetMapping("/{id}")
+    public Tag findById(@PathVariable("id") Long id) {
+        return this.tagService.findById(id);
+    }
+
+    /** UPDATE **/
+
+    @PutMapping("/{id}")
+    public Tag update(@PathVariable("id") Long id, @RequestBody @Valid TagDto dto) {
+        return this.tagService.update(id, dto);
+    }
+
     /** CREATE **/
 
     @PostMapping()
     public Tag create(@RequestBody @Valid TagDto dto) {
         return this.tagService.create(dto);
+    }
+
+    /** DELETE **/
+
+    @DeleteMapping("/{id}") 
+    public void deleteById(@PathVariable("id") Long id) {
+        this.tagService.deleteById(id);
     }
     
 }
--- src/main/java/fr/perso/skillcheck/tag/TagService.java
+++ src/main/java/fr/perso/skillcheck/tag/TagService.java
@@ -1,20 +1,50 @@
 package fr.perso.skillcheck.tag;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import fr.perso.skillcheck.exceptions.NotFoundException;
 import fr.perso.skillcheck.tag.dto.TagDto;
 
 @Service
 public class TagService {
 
     @Autowired
-    private TagRepository   tagRepository;
+    private TagRepository tagRepository;
+
+    /** FIND ALL **/
+
+    public List<Tag> findAll() {
+        return this.tagRepository.findAll();
+    }
+
+    /** FIND **/
+
+    public Tag findById(Long id) {
+        return this.tagRepository.findById(id).orElse(null);
+    }
+
+    /** UPDATE **/
+
+    public Tag update(Long id, TagDto dto) {
+        Tag tag = this.tagRepository.findById(id).orElseThrow(() -> new NotFoundException("No tag found with id " + id));
+        tag.setLabel(dto.getLabel());
+    
+        return this.tagRepository.save(tag);
+    }
 
     /** CREATE **/
 
     public Tag create(TagDto dto) {
         Tag tag = new Tag(dto);
         return this.tagRepository.save(tag);
+    }
+
+    /** DELETE **/
+
+    public void deleteById(Long id) {
+        this.tagRepository.deleteById(id);
     }
 }

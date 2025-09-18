32ca64979ae6b325a59773a8082716199cb445fb
mehdi-larek <larek.med@gmail.com>
2025-09-18 10:29:24 +0200

    filtre sur tagId dans la liste des questions

--- src/main/java/fr/perso/skillcheck/question/filter/QuestionFilter.java
+++ src/main/java/fr/perso/skillcheck/question/filter/QuestionFilter.java
@@ -7,21 +7,27 @@ import org.springframework.data.jpa.domain.Specification;
 
 import fr.perso.skillcheck.constants.Difficulty;
 import fr.perso.skillcheck.question.Question;
+import fr.perso.skillcheck.questionHasTag.QuestionHasTag;
 import fr.perso.skillcheck.utils.GenericFilter;
 import fr.perso.skillcheck.utils.UtilEntity;
 import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import jakarta.persistence.criteria.Subquery;
 
 public class QuestionFilter extends GenericFilter{
 
     private Difficulty          difficulty;
 
+    private Long                tagId;
+
     public QuestionFilter() {
         super();
     }
 
-    public QuestionFilter(Difficulty difficulty) {
+    public QuestionFilter(Difficulty difficulty, Long tagId) {
         super();
         this.difficulty = difficulty;
+        this.tagId = tagId;
     }
 
     /** DIFFICULTY **/
@@ -38,6 +44,20 @@ public class QuestionFilter extends GenericFilter{
         return !UtilEntity.isEmpty(this.difficulty);
     }
 
+    /** TAG ID **/
+
+    public Long getTagId() {
+        return this.tagId;
+    }
+
+    public void setTagId(Long tagId) {
+        this.tagId = tagId;
+    }
+
+    public boolean hasTagId() {
+        return !UtilEntity.isEmpty(this.tagId);
+    }
+
     /** METHODS **/
 
     public Specification<Question> toSpecification() {
@@ -49,6 +69,14 @@ public class QuestionFilter extends GenericFilter{
 
             if (this.hasDifficulty()) {
                 predicates.add(cb.equal(root.get("difficulty"), this.getDifficulty()));
+            }
+
+            if (this.hasTagId() && query != null) {
+                Subquery<Long> sq = query.subquery(Long.class);
+                Root<QuestionHasTag> qht = sq.from(QuestionHasTag.class);
+                sq.select(qht.get("question").get("id"))
+                .where(cb.equal(qht.get("tag").get("id"), this.getTagId()));
+                predicates.add(root.get("id").in(sq));
             }
 
             //exemple
